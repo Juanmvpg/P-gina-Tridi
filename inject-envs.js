91fcfc11bd0d4b7d6c6fa8bae365303c58dcd6fe
Juan <juanmartinpintos@gmail.com>
2026-05-24 11:48:50 -0300

    Hacer editables los links de videotutoriales, presentaciones y contenido completo vía variables de entorno en Netlify

--- inject-envs.js
+++ inject-envs.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const files = ['index.html'];
+const files = ['index.html', 'recursos.html', 'lineamientos.html'];
 
 files.forEach(file => {
   const filePath = path.join(__dirname, file);
@@ -11,14 +11,16 @@ files.forEach(file => {
     const envs = {
       'URL_NEWSLETTER': process.env.URL_NEWSLETTER || 'https://proyectotridi.my.canva.site/',
       'URL_TINKERCAD': process.env.URL_TINKERCAD || 'https://www.tinkercad.com/',
-      'URL_FILMINAS': process.env.URL_FILMINAS || 'https://canva.link/kuge1m3si5tnzl6'
+      'URL_FILMINAS': process.env.URL_FILMINAS || 'https://canva.link/kuge1m3si5tnzl6',
+      'URL_VIDEOTUTORIALES': process.env.URL_VIDEOTUTORIALES || 'https://drive.google.com/drive/folders/1O82aKVfCP3JqR8bUhE4y9Jy9My5HXZmu?usp=sharing',
+      'URL_PRESENTACIONES': process.env.URL_PRESENTACIONES || 'https://docs.google.com/document/d/133DJ-1A8vmaM4WfvWOOMq3ZHMEIoHd3sP47rxDQYvY8/edit?usp=sharing',
+      'URL_CONTENIDO_COMPLETO': process.env.URL_CONTENIDO_COMPLETO || 'https://drive.google.com/drive/folders/1Z8ams6Vv-Mt8fqJnFdD2GGf4McPxVfRF?usp=sharing'
     };
 
     console.log(`Processing ${file}...`);
     Object.keys(envs).forEach(key => {
       const placeholder = `%${key}%`;
       const value = envs[key];
-      // Use split/join to replace all occurrences safely across Node versions
       content = content.split(placeholder).join(value);
       console.log(`  Replacing ${placeholder} with: ${value}`);
     });
