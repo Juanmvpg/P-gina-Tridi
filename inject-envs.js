68b00a990ab9f4593f4aa57ceec881742b5452df
Juan <juanmartinpintos@gmail.com>
2026-06-01 15:07:15 -0300

    Implementar sistema de acceso restringido, panel de control dinámico con cronograma y guías de configuración

--- inject-envs.js
+++ inject-envs.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const files = ['index.html', 'recursos.html', 'lineamientos.html'];
+const files = ['index.html', 'recursos.html', 'lineamientos.html', 'panel.html', 'login.html'];
 
 files.forEach(file => {
   const filePath = path.join(__dirname, file);
@@ -14,7 +14,9 @@ files.forEach(file => {
       'URL_FILMINAS': process.env.URL_FILMINAS || 'https://canva.link/kuge1m3si5tnzl6',
       'URL_VIDEOTUTORIALES': process.env.URL_VIDEOTUTORIALES || 'https://drive.google.com/drive/folders/1O82aKVfCP3JqR8bUhE4y9Jy9My5HXZmu?usp=sharing',
       'URL_PRESENTACIONES': process.env.URL_PRESENTACIONES || 'https://docs.google.com/document/d/133DJ-1A8vmaM4WfvWOOMq3ZHMEIoHd3sP47rxDQYvY8/edit?usp=sharing',
-      'URL_CONTENIDO_COMPLETO': process.env.URL_CONTENIDO_COMPLETO || 'https://drive.google.com/drive/folders/1Z8ams6Vv-Mt8fqJnFdD2GGf4McPxVfRF?usp=sharing'
+      'URL_CONTENIDO_COMPLETO': process.env.URL_CONTENIDO_COMPLETO || 'https://drive.google.com/drive/folders/1Z8ams6Vv-Mt8fqJnFdD2GGf4McPxVfRF?usp=sharing',
+      'URL_SPREADSHEET': process.env.URL_SPREADSHEET || 'https://docs.google.com/spreadsheets/d/1SxucJUDXjgqRuSRcbtFh-zy4OnVYOoqsz67SCetbN0A/export?format=csv',
+      'URL_CRONOGRAMA': process.env.URL_CRONOGRAMA || 'https://docs.google.com/spreadsheets/d/1e3bOS0IgtFsuKGqu28vhPJORfwWV29A7wyfTYnGibEA/export?format=csv'
     };
 
     console.log(`Processing ${file}...`);
