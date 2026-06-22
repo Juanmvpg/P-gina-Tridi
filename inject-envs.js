0053d8be0d784744ab7cd6e801fd6c144c4787a4
Juanmvpg <juanmartinpintos@gmail.com>
2026-06-22 11:14:02 -0300

    Actualizar hoja de cálculo de horas totales y reajustar columnas de DNI y horas en login y panel

--- inject-envs.js
+++ inject-envs.js
@@ -15,7 +15,7 @@ files.forEach(file => {
       'URL_VIDEOTUTORIALES': process.env.URL_VIDEOTUTORIALES || 'https://drive.google.com/drive/folders/1O82aKVfCP3JqR8bUhE4y9Jy9My5HXZmu?usp=sharing',
       'URL_PRESENTACIONES': process.env.URL_PRESENTACIONES || 'https://docs.google.com/document/d/133DJ-1A8vmaM4WfvWOOMq3ZHMEIoHd3sP47rxDQYvY8/edit?usp=sharing',
       'URL_CONTENIDO_COMPLETO': process.env.URL_CONTENIDO_COMPLETO || 'https://drive.google.com/drive/folders/1Z8ams6Vv-Mt8fqJnFdD2GGf4McPxVfRF?usp=sharing',
-      'URL_SPREADSHEET': process.env.URL_SPREADSHEET || 'https://docs.google.com/spreadsheets/d/1SxucJUDXjgqRuSRcbtFh-zy4OnVYOoqsz67SCetbN0A/export?format=csv',
+      'URL_SPREADSHEET': process.env.URL_SPREADSHEET || 'https://docs.google.com/spreadsheets/d/1SZLoY_iCSnatl3MCIFkP5zsHdtv4Cl_EFrzpm_vPR_c/export?format=csv',
       'URL_CRONOGRAMA': process.env.URL_CRONOGRAMA || 'https://docs.google.com/spreadsheets/d/1e3bOS0IgtFsuKGqu28vhPJORfwWV29A7wyfTYnGibEA/export?format=csv',
       'URL_REPORTE_FALLAS': process.env.URL_REPORTE_FALLAS || 'https://docs.google.com/forms/d/e/1FAIpQLSca1WG1YBkjE8jMsJWqLb9yDkMH1VgWLalM_NMo5jezxbMOzQ/viewform?usp=publish-editor',
       'URL_KAHOOT': process.env.URL_KAHOOT || 'https://kahoot.it/',
